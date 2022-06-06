077841cd72749d83bdb7f12bfc064843fd76da5f
YK-Omamori <mars880928@gmail.com>
2022-06-07 01:34:12 +0800

    Simplified(?) NotifCounter
    Co-Authored-By: Penny Ye <106177586+YPC20@users.noreply.github.com>

--- NotifCounter.py
+++ NotifCounter.py
@@ -3,44 +3,25 @@
 import datetime
 
 class NotifCounter:
-    remind_day = 3
 
-    def __init__(self, course, task, deadline):  # Should be obtained from crawler
-        self.course = course
-        self.task = task
-        self.deadline = deadline
-
-
-    def cur_datetime(self):
-        get_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        cur_datetime = datetime.datetime.strptime(get_datetime, "%Y-%m-%d %H:%M:%S")
-        return cur_datetime
+    def __init__(self, data_list, remind_day):
+        self.data_list = data_list
+        self.remind_day = remind_day
 
 
     def notif_or_not(self):
-        notif_dict = dict()
-        notif_range = datetime.timedelta(days=NotifCounter.remind_day)  # Should be obtained from Setting
-
-        deadline = datetime.datetime.strptime(self.deadline, "%Y-%m-%dT%H:%M:%SZ") # Should be consistent with the datatype from crawler
-        if deadline - self.cur_datetime() <= notif_range:
-            if notif_dict.get(self.course) == None:
-                notif_dict[self.course] = self.task
-            else:
-                notif_dict[self.course] += self.task
-        return notif_dict
+        return_list = []
+        notif_range = datetime.timedelta(days=self.remind_day)
+        for one_assi in self.data_list:
+            if one_assi.deadline - datetime.datetime.now() <= notif_range:
+                return_list.append(one_assi)
+        return return_list
 
 
 """
-    def auto_refresh(self):
-        refr_period = datetime.timedelta(hours=1)  # To be discussed
-        init_time = "2022-06-03 17:26:20"  # Should be obtained from Setting
-
-       while True:
-        return 
-"""
-
-
 if __name__ == "__main__":
-    n = NotifCounter("PBC", "HW1", "2022-06-06T12:30:00Z")
+    n = NotifCounter(, 3)
     n = n.notif_or_not()
     print(n)
+# trying to recreate a Assignment(course, task, deadline) was a disaster
+"""
