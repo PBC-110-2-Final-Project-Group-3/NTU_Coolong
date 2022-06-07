b0f628a2bb4cb0d08dce95021eb813bb135d2d94
flora-hsieh <105841522+flora-hsieh@users.noreply.github.com>
2022-06-07 21:34:38 +0800

    Update_Window
    datetime/zoning/list update

--- window.py
+++ window.py
@@ -1,63 +1,43 @@
-'''
-現在有 n 門課、4個column
-課程id有id1 ... idn
-課程有x1 ... xn
-內容有y1 ... yn
-日期有z1 ... zn
-時間有t1 ... tn
+import datetime
+from tkinter import *
+from  tkinter import ttk
 
-希望內容欄位的寬度可以隨內容長度變化
-希望整個視窗的高度可以隨課程數(n)變化
-
-'''
 if __name__ == "__main__":
     # parameters(temporary)
 
-    n = 4
-
+    '''
     course = ['C/C++程式設計 C/C++ Programming',
             '作業研究 Operations Research',
             '應用線性代數 Applied Linear Algebra',
             '應用線性代數II Applied Linear AlgebraII']
-    course_n_assignment = {'C/C++程式設計 C/C++ Programming':['Homework 1','Homework 2'],
-            '作業研究 Operations Research':['Case Assignment 1'],
-            '應用線性代數 Applied Linear Algebra':['Final Exam'],
+    course_n_assignment = {'C/C++程式設計 C/C++ Programming':'Homework 1','C/C++程式設計 C/C++ Programming':'Homework 2',
+            '作業研究 Operations Research':'Case Assignment 1',
+            }
+    course_n_quizz = {'應用線性代數 Applied Linear Algebra':['Final Exam'],
             '應用線性代數II Applied Linear AlgebraII':['quiz1']}
+    assignment = ['C/C++程式設計 C/C++ Programming,Homework 1,2021-10-13 17:30:00','C/C++程式設計 C/C++ Programming,Homework 2,2021-11-10 17:30:00',
+    '作業研究 Operations Research,Case Assignment 1,2021-12-08 17:30:00']
+    quiz = ['應用線性代數 Applied Linear Algebra,Final Exam, 2022-01-01 22:00:00',
+            '應用線性代數II Applied Linear AlgebraII,quiz1,2022-02-21 23:59:59']
+    '''
+    
+    # temporary deadline
+    deadline = datetime.datetime(2022, 6, 19, 23, 59, 59)
+    dates = deadline.date()
+    times = deadline.time()
+    category = 0
 
-
-# input
-# n = int(input())
-# course = []
-# for i in range(n):
-    # course.append(input("課程名稱："))
-# assignment = []
-# for i in range(n):
-    # assignment.append(input("作業或考試：").split(','))
-# course_n_assignment = dict()
-# for i in range(n):
-    # course_n_assignment[course[i]] = assignment[i]
-# print(course_n_assignment)  # 到時id_n_classes可以被_get_course_id()取代掉
-
-if __name__ == "__main__":
-    dates = []
-    for i in range(n):
-        dates.append(input("截止日期："))
-    times = []
-    for i in range(n):
-        times.append(input("截止時間："))
     
 
 
 
 # settings
-from tkinter import *
-from  tkinter import ttk
-
 class window(Tk):
 
-    def __init__(self, course, course_n_assignment, dates, times):
+    def __init__(self, category, course, course_n_assignment, dates, times):
         
         Tk.__init__(self)
+        self.category = category
         self.course = course
         self.course_n_assignment = course_n_assignment
         self.dates = dates
@@ -66,7 +46,7 @@ class window(Tk):
         self.title('DeadLines')
         self.list()
 
-    # root = Tk()
+        
     # root.title('DeadLines')
 
     def list(self):    
@@ -75,7 +55,7 @@ class window(Tk):
         style.theme_use("clam")
         style.configure("Treeview.Heading", background="beige")
         style.map('Treeview', background=[('selected','darkseagreen')])
-
+        # root = tk.Tk()
 
 # root.geometry('500x300')
 
@@ -84,8 +64,9 @@ class window(Tk):
 
 
 
-        set['columns']= ('course','assignment','date', 'time')
+        set['columns']= ('category','course','assignment','date', 'time')
         set.column("#0", width=0, stretch=NO)
+        set.column("category",anchor=CENTER, width=70)
         set.column("course",anchor=CENTER)
         set.column("assignment",anchor=CENTER)
         set.column("date",anchor=CENTER, width=60)
@@ -93,6 +74,7 @@ class window(Tk):
 
         # header
         set.heading("#0",text="",anchor=CENTER)
+        set.heading("category",text="Category",anchor=CENTER)
         set.heading("course",text="Class",anchor=CENTER)
         set.heading("assignment",text="Content",anchor=CENTER)
         set.heading("date",text="Date",anchor=CENTER)
@@ -100,10 +82,22 @@ class window(Tk):
 
 
         # output
+        assignments = []
+        for i in range(len(assignment)):
+            assignments.append(assignment[i].split(','))
+        quizzes = []
+        for i in range(len(quiz)):
+            quizzes.append(quiz[i].split(','))
 
-        for i in range(n):
-            set.insert(parent='',index='end',iid=i,text='',values=(course[i],course_n_assignment.get(course[i]),dates[i],times[i]))
+        # Label(self, text="作業", bg="#3300CC", fg="white").place(x=90, y=28)
+        # Label(self, text="Position 1 : x=0, y=0", bg="#FFFF00", fg="white")
+        set.insert(parent='',index='end',text='',values=("作業"))
+        for i in range(len(assignment)):
+            set.insert(parent='',index='end',iid=i,text='',values=(" ",assignments[i][0],assignments[i][1],dates,times))
+        set.insert(parent='',index='end',text='',values=("考試"))
+        for j in range(len(quiz)):
+            set.insert(parent='',index='end',text='',values=(" ",quizzes[j][0],quizzes[j][1],dates,times))
 
 if __name__ == "__main__":
-    win = window(course, course_n_assignment, dates, times)
+    win = window(category, course, course_n_assignment, dates, times)
     win.mainloop()
