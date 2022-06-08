522fcd20ee97f83c024fa3f7cdcd99821b92e205
flora-hsieh <105841522+flora-hsieh@users.noreply.github.com>
2022-06-08 23:08:57 +0800

    Update Setting.py

--- Setting.py
+++ Setting.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 import yaml
 import os
-<<<<<<< HEAD
+
 
 '''這個檔案是「首次登入」介面，按登入button之後會把使用者輸入的帳號密碼存在一個叫Config.yaml的檔案裡'''
 class Setting(tk.Tk):
@@ -57,50 +57,7 @@ class Setting(tk.Tk):
 	def leave(self):
 		self.exit = True
 
-=======
-'''這個檔案是「首次登入」介面，按登入button之後會把使用者輸入的帳號密碼存在一個叫Config.yaml的檔案裡'''
-class Setting(tk.Tk):
-   def __init__(self, *args, **kwargs):
-	tk.Tk.__init__(self, *args, **kwargs)
-	self.title('NTU COOLONG LOGIN')
-	self.geometry('380x130+550+300')
-	self.resizable(width=0, height=0)
-	self.grid()
-	self.createWidgets()
-	self.arrange()
-	self.configure(bg='beige')
-	self.exit = False
-    def createWidgets(self):
-	self.userLabel = tk.Label(self, text="請輸入帳號：", bg='beige')
-	self.passwordLabel = tk.Label(self, text="請輸入密碼：", bg='beige')
-	self.userEntry = tk.Entry(self,highlightbackground='beige',bg='beige')
-	self.passwordEntry = tk.Entry(self, show='*',highlightbackground='beige',bg='beige')
-	self.remindDaysLabel = tk.Label(self, text="在作業截止前", bg='beige')
-	self.Days = tk.Entry(self, width=3,highlightbackground='beige',bg='beige',justify='center')
-	self.Days.insert(0,'3') # 預設3天前提醒
-	self.remindDaysLabel2 = tk.Label(self, text="天提醒我", bg='beige')
-	self.loginBtn = tk.Button(self, text="登入", width=10, highlightbackground='beige',fg='darkseagreen', command=lambda:[self.save()])
-	self.leaveBtn = tk.Button(self, text='離開',width=10, highlightbackground='beige', fg='darkseagreen',command=lambda:[self.destroy(),self.leave()])
-    def arrange(self):
-	self.userLabel.grid(row=1, column=1, sticky='E')
-	self.passwordLabel.grid(row=2, column=1, sticky='E')
-	self.userEntry.grid(row=1, column=2, sticky='W', columnspan=4)
-	self.passwordEntry.grid(row=2, column=2, sticky='W', columnspan=4)
-	self.remindDaysLabel.grid(row=3, column=1,sticky='E')
-	self.Days.grid(row=3, column=2, sticky='W'+'E')
-	self.remindDaysLabel2.grid(row=3, column=3, sticky='W')
-	self.loginBtn.grid(row=5, column=3,rowspan=2,sticky='N')
-	self.leaveBtn.grid(row=5, column=2,rowspan=2,sticky='N')
-	self.grid_rowconfigure(4, minsize=13)
-    def save(self): # 儲存帳號密碼、倒數幾天
-	d = {}
-	d['account'] = self.userEntry.get()
-	d['password'] = self.passwordEntry.get()
-	d['remindDay'] = self.Days.get()
-	path = os.path.join('Users', 'pengchihyi', 'Desktop','PBC', 'NTU_Coolong')
-    def leave(self):
-	self.exit = True
->>>>>>> 44a2066ffb29ff255def603ef99674940344d8f3
+
 if __name__ == "__main__":
 	Set = Setting()
 	Set.mainloop()
