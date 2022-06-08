ac4960b8e663828b9f092661f7834b5fb330f237
flora-hsieh <105841522+flora-hsieh@users.noreply.github.com>
2022-06-08 22:44:08 +0800

    Update window.py

--- window.py
+++ window.py
@@ -82,7 +82,7 @@ class window(Tk):
             for j in range(len(self.quizzes)):
                 set.insert(parent='',index='end',text='',values=(" ",self.quizzes[j].course,self.quizzes[j].name,self.quizzes[j].deadline))
         self.leaveBtn = tk.Button(self, text='Refresh',width=10, highlightbackground='beige', fg='darkseagreen',command=lambda:[self.destroy(),self.leave()])
-		self.exit = True   
+	self.exit = True   
 
 if __name__ == "__main__":
     win = window(category, course, name, deadline)
